8fae2f3c9c895ffe9aef35584fae62d4dfd33a78
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-06-07 20:33:56 +0200

    ✨ Initial commit: Set up CineMate Xcode project with gitignore and assets

---
+++ CineMate/CineMate/CineMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineMateApp.swift
+//  CineMate
+//
+//  Created by Nicholas Samuelsson Jeria on 2025-06-07.
+//
+
+import SwiftUI
+
+@main
+struct CineMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
