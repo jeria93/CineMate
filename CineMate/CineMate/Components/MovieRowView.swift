9430fad3aedbdc09a97fbb32e69e27f9d1ce020c
jeria93 <39989194+jeria93@users.noreply.github.com>
2025-06-08 20:37:52 +0200

    feat(ui): added compne
    - implemented MovieRowDetails to keep MovieRowView cleaner(less code)
    - MovieRowDetails as reusable components
    - MovieList as new entry point
    - Moved previews to its own file, see different scenarios

--- CineMate/CineMate/Components/MovieRowView.swift
+++ CineMate/CineMate/Components/MovieRowView.swift
@@ -11,41 +11,10 @@ struct MovieRowView: View {
     let movie: Movie
 
     var body: some View {
-        HStack(alignment: .top, spacing: 16) {
-
+        HStack(alignment: .top, spacing: 15) {
             PosterImageView(url: movie.posterURL, title: movie.title)
-
-//            Extrac? MovieRowDetails?
-            VStack(alignment: .leading, spacing: 5) {
-
-                Text(movie.title)
-                    .font(.headline)
-
-                if let overview = movie.overview {
-                    Text(overview)
-                        .font(.subheadline)
-                        .lineLimit(3)
-                        .foregroundColor(.secondary)
-                }
-
-                if let releaseDate = movie.realeaseDate {
-                    Text("Release: \(releaseDate)")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                }
-
-                if let voteAverage = movie.voteAverage {
-                    HStack(spacing: 4) {
-                        Image(systemName: "star.fill")
-                            .foregroundColor(.yellow)
-                            .font(.caption)
-
-                        Text(String(format: "%.1f", voteAverage))
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
-                }
-            }
+            MovieRowDetails(movie: movie)
+            Spacer()
         }
         .padding(.vertical, 8)
         .background(Color(.systemBackground))
@@ -55,11 +24,5 @@ struct MovieRowView: View {
 }
 
 #Preview {
-//    Create more previews on different scenarios?
     MovieRowView(movie: PreviewData.starWars)
 }
-// Skriv om hela sen, knappa allt från början
-// Finns det fler scenarion tex, ej poster, eller ej title etc etc? skapa previews?
-// skapa klart med list som gpt visade
-// fråga, vad kan vi refaktorisera här? eller extrahera?
-// Vad vill man visa upp i MovieRowView?
